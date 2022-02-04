4614498d309d5d3635ed6bedc5cc4bc6ba0e10f8
mthous72 <mthous01@hotmail.com>
2022-02-04 09:57:29 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -10,7 +10,7 @@ COMMON_LUNCH_CHOICES := \
     aosp_dre8-eng \
     lineage_dre8-user \
     lineage_dre8-userdebug \
-    lineage_dre8-eng
+    lineage_dre8-eng \
     derpfest_dre8-user \
     derpfest_dre8-userdebug \
     derpfest_dre8-eng
